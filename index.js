98c258bfd010582d7f8675c6e1a050481e78e5ad
Rob Paik <robpaik@gmail.com>
2019-06-16 02:57:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,3 +4,11 @@ var kittens = ["Milo", "Otis", "Garfield"] //define your array here
 function destructivelyAppendKitten (name) {
   kittens.push(name)
 }
+
+function destructivelyPrependKitten (name) {
+  kittens.unshift(name)
+}
+
+function destructivelyRemoveLastKitten (name) {
+  kittens.pop()
+}
